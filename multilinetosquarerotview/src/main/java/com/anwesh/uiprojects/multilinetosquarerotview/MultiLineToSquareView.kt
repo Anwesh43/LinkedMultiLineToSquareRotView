fb064ce07e64f11b76e280d97f885cce714be745
Anwesh43 <anweshthecool0@gmail.com>
2019-07-07 17:57:47 +0530

    adding Int and Float extension methods

--- multilinetosquarerotview/src/main/java/com/anwesh/uiprojects/multilinetosquarerotview/MultiLineToSquareView.kt
+++ multilinetosquarerotview/src/main/java/com/anwesh/uiprojects/multilinetosquarerotview/MultiLineToSquareView.kt
@@ -22,3 +22,14 @@ val foreColor : Int = Color.parseColor("#1A237E")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val sweepDeg : Float = 90f
 
+fun Int.inverse() : Float = 1f / this
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.mirrorValue(a : Int, b : Int) : Float {
+    val k : Float = scaleFactor()
+    return (1 - k) * a.inverse() + k * b.inverse()
+}
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float {
+    return mirrorValue(a, b) * dir * scGap
+}
