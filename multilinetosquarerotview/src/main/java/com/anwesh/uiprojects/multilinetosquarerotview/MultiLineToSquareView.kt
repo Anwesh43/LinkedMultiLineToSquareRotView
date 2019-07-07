5f12b7ebab9147cc0932cc6f3007afb591df64c4
Anwesh43 <anweshthecool0@gmail.com>
2019-07-07 18:18:17 +0530

    adding MultiLineToSquare class

--- multilinetosquarerotview/src/main/java/com/anwesh/uiprojects/multilinetosquarerotview/MultiLineToSquareView.kt
+++ multilinetosquarerotview/src/main/java/com/anwesh/uiprojects/multilinetosquarerotview/MultiLineToSquareView.kt
@@ -177,4 +177,28 @@ class MultiLineToSquareView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class MultiLineToSquare(var i : Int) {
+
+        private val root : MLTSRNode = MLTSRNode(0)
+        private var curr : MLTSRNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
