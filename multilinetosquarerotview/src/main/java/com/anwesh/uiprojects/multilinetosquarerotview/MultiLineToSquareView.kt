4c57c251d7a08b8794279b3df6160e208ebac4e1
Anwesh43 <anweshthecool0@gmail.com>
2019-07-07 18:16:00 +0530

    adding MLTSRNode class

--- multilinetosquarerotview/src/main/java/com/anwesh/uiprojects/multilinetosquarerotview/MultiLineToSquareView.kt
+++ multilinetosquarerotview/src/main/java/com/anwesh/uiprojects/multilinetosquarerotview/MultiLineToSquareView.kt
@@ -132,5 +132,49 @@ class MultiLineToSquareView(ctx : Context) : View(ctx) {
                 animated = false
             }
         }
-    } 
+    }
+
+    data class MLTSRNode(var i : Int, val state : State = State()) {
+
+        private var next : MLTSRNode? = null
+        private var prev : MLTSRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = MLTSRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMLTSRNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MLTSRNode {
+            var curr : MLTSRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
