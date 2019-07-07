a27ff699ea1975f79a7a9fbfc0fc3323473df061
Anwesh43 <anweshthecool0@gmail.com>
2019-07-07 18:07:55 +0530

    adding MultiLineToSquareView class

--- multilinetosquarerotview/src/main/java/com/anwesh/uiprojects/multilinetosquarerotview/MultiLineToSquareView.kt
+++ multilinetosquarerotview/src/main/java/com/anwesh/uiprojects/multilinetosquarerotview/MultiLineToSquareView.kt
@@ -68,3 +68,21 @@ fun Canvas.drawMLTSRNode(i : Int, scale : Float, paint : Paint) {
     drawMultiLineSquare(sc1, sc2, size, paint)
     restore()
 }
+
+class MultiLineToSquareView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                return true
+            }
+        }
+        return true
+    }
+}
