09ade8a9442919c75aa63b70f2b40d7199d7a2a4
Anwesh43 <anweshthecool0@gmail.com>
2019-07-07 13:30:15 +0530

    adding constants and libraries

--- multilinetosquarerotview/src/main/java/com/anwesh/uiprojects/multilinetosquarerotview/MultiLineToSquareView.kt
+++ multilinetosquarerotview/src/main/java/com/anwesh/uiprojects/multilinetosquarerotview/MultiLineToSquareView.kt
@@ -3,3 +3,22 @@ package com.anwesh.uiprojects.multilinetosquarerotview
 /**
  * Created by anweshmishra on 07/07/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#1A237E")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val sweepDeg : Float = 90f
+
